8b0382bfe0ed0a10886e0346296d0da658da2dd9
Roy Bekkering <roytazz@gmail.com>
2015-12-28 00:48:26 +0100

    CHANGED - Large request is adjustable

--- GuildWars2API/Network/NetworkManager.cs
+++ GuildWars2API/Network/NetworkManager.cs
@@ -7,7 +7,7 @@ namespace GuildWars2API.Network
 {
     public static class NetworkManager
     {
-        private const int MAX_ITEMS_PER_REQUEST = 50;
+        private const int MAX_ITEMS_PER_REQUEST = 100;
 
         /// <summary>
         /// Reads the world wide web
@@ -18,52 +18,29 @@ namespace GuildWars2API.Network
 
         public static string AuthorizedRequest(string url, string apiKey) => GetResponse(url, apiKey);
 
-        private static string GetResponse(string url, string apiKey = null) {
-            try {
-                using(WebClient client = new WebClient()) {
-                    if(apiKey != null) {
-                        client.Headers.Add("Authorization", $"Bearer {apiKey}");
-                    }
-
-                    Console.WriteLine("[INFO] Loading... " + url);
-                    DateTime start = DateTime.Now;
-
-                    string result = client.DownloadString(url);
-
-                    Console.WriteLine("[INFO] Response Time " + (DateTime.Now - start).TotalSeconds + "sec");
-
-                    return result;
-                }
-            }
-            catch(Exception ex) {
-                Console.WriteLine("[ERROR] " + ex.Message);
-                return string.Empty;
-            }
-        }
-
-        public static List<T> GetLargeRequest<T>(List<int> IDs, string apiCategory, string APIKey = null) {
+        public static List<T> GetLargeRequest<T>(List<int> IDs, string APIEndpoint, int itemsPerRequest = MAX_ITEMS_PER_REQUEST, string APIKey = null) {
             List<T> results = new List<T>();
             if(IDs.Count <= 0) {
                 return results;
             }
 
             //Devide collection in smaller parts
-            if(IDs.Count > MAX_ITEMS_PER_REQUEST) {
+            if(IDs.Count > itemsPerRequest) {
                 List<int> selected = new List<int>();
 
-                selected.AddRange(IDs.GetRange(MAX_ITEMS_PER_REQUEST, IDs.Count - MAX_ITEMS_PER_REQUEST));
-                IDs.RemoveRange(MAX_ITEMS_PER_REQUEST, IDs.Count - MAX_ITEMS_PER_REQUEST);
+                selected.AddRange(IDs.GetRange(itemsPerRequest, IDs.Count - itemsPerRequest));
+                IDs.RemoveRange(itemsPerRequest, IDs.Count - itemsPerRequest);
 
-                results.AddRange(GetLargeRequest<T>(selected, apiCategory));
+                results.AddRange(GetLargeRequest<T>(selected, APIEndpoint));
             }
 
             //Authorized or Unauthorized Request
             string response = "";
             if(APIKey == null) {
-                response = UnauthorizedRequest(URLBuilder.LargeRequestURL(apiCategory, IDs));
+                response = UnauthorizedRequest(URLBuilder.LargeRequestURL(APIEndpoint, IDs));
             }
             else {
-                response = AuthorizedRequest(URLBuilder.LargeRequestURL(apiCategory, IDs), APIKey);
+                response = AuthorizedRequest(URLBuilder.LargeRequestURL(APIEndpoint, IDs), APIKey);
             }
 
             //Read response
@@ -71,6 +48,29 @@ namespace GuildWars2API.Network
                 results.AddRange(JsonConvert.DeserializeObject<List<T>>(response));
             }
             return results;
+        }
+        
+        private static string GetResponse(string url, string apiKey = null) {
+            try {
+                using(WebClient client = new WebClient()) {
+                    if(apiKey != null) {
+                        client.Headers.Add("Authorization", $"Bearer {apiKey}");
+                    }
+#if DEBUG
+                    Console.WriteLine("[INFO] Loading... " + url);
+                    DateTime start = DateTime.Now;
+#endif
+                    string result = client.DownloadString(url);
+#if DEBUG
+                    Console.WriteLine("[INFO] Response Time " + (DateTime.Now - start).TotalSeconds + "sec");
+#endif
+                    return result;
+                }
+            }
+            catch(Exception ex) {
+                Console.WriteLine("[ERROR] " + ex.Message);
+                return string.Empty;
+            }
         }
     }
 }
