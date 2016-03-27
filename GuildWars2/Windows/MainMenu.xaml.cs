ba0d3ec9ed0a517c87adeb64c3c9eb596334b572
Roy Bekkering <roytazz@gmail.com>
2016-03-27 04:34:55 +0200

    ADDED - New concept main menu

---
+++ GuildWars2/Windows/MainMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace GuildWars2.Windows
+{
+    public partial class MainMenu : MetroWindow
+    {
+        public MainMenu() {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_ContextMenuOpening(object sender, System.Windows.Controls.ContextMenuEventArgs e) {
+            e.Handled = false;
+        }
+    }
+}
