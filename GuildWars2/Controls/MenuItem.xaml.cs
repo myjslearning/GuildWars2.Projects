562be54b5a8b342ff30a22b712e69d4a814ef5b0
Roy Bekkering <roytazz@gmail.com>
2016-04-09 00:29:13 +0200

    ADDED - New menu

---
+++ GuildWars2/Controls/MenuItem.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GuildWars2.Controls
+{
+    /// <summary>
+    /// Interaction logic for MenuItem.xaml
+    /// </summary>
+    public partial class MenuItem : UserControl
+    {
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(MenuItem), new PropertyMetadata(""));
+        
+        public VisualBrush Icon
+        {
+            get { return (VisualBrush)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(VisualBrush), typeof(MenuItem), new PropertyMetadata(null));
+        
+        public MenuItem() {
+            InitializeComponent();
+        }
+    }
+}
