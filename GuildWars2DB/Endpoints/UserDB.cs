f462dea8955000ef5cf74b2d300f7801e6b68db7
Roy Bekkering <roytazz@gmail.com>
2016-04-17 01:37:37 +0200

    ADDED - DB functions for website

---
+++ GuildWars2DB/Endpoints/UserDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuildWars2DB.EndPoints
+{
+    public static class UserDB
+    {
+        public static bool IsValid(string username, string password) {
+            throw new NotImplementedException();
+        }
+
+        public static void RegisterUser(string username, string password) {
+            throw new NotImplementedException();
+        }
+
+        public static void DeleteUser(string username) {
+            throw new NotImplementedException();
+        }
+
+        public static Dictionary<string, string> GetUser(string username) {
+            throw new NotImplementedException();
+        }
