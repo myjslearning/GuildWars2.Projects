f54c495b5334fa967484562a22a4258470e225c5
Roy Bekkering <roytazz@gmail.com>
2016-04-19 01:22:33 +0200

    ADDED - New project for a special Guild application

---
+++ GuildWars2.Guild/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MahApps.Metro.Controls;
+
+namespace GuildWars2Guild
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
